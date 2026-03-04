e685b9b3667240b4c0c186e57bd186f74c006ed8
Stone Casey <Stonecasey02@gmail.com>
2026-03-04 14:28:16 -0500

    Removed Joe from Team | fixed CSS for Team cards

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,7 +4,6 @@ import styles from './Team.module.css'
 import StoneImage from '../assets/Stone.jpg'
 import SamImage from '../assets/Sam.jpg'
 import JordanImage from '../assets/Jordan.jpg'
-import JoeImage from '../assets/Joe.jpg'
 import AaronImage from '../assets/Aaron.jpg'
 import BlaineImage from '../assets/Blaine.jpg'
 
@@ -28,21 +27,18 @@ export default function Team(){
                 role="ROLE NEEDED" 
                 bio="Blaine is  a Senior Computer Science major at ODU. He is interested in pursuing a career in software engineering after graduation. In his free time, Blaine enjoys both playing and developing video games."
                 src={BlaineImage}/>
-            <Member 
-                name="Aaron Breslin" 
-                role="Backend Developer" 
-                bio="Aaron is a Senior Computer Science major at ODU. He is also studying for a minor in Computer Engineering. In his free time he enjoys reading books and playing both board games and video games with friends."
-                src={AaronImage}/>
-            <Member 
-                name="Joseph Thompson" 
-                role="Documentation Specialist" 
-                bio="Joseph is a Senior Computer Science major at ODU. He has already earned a Masters in Electrical Engineering (ODU Spring 2019). He works full time in DRAM manufacturing as a Senior Engineer. When he's not working or studying, he's behind the wheel of his GR86 doing Autocross events, or he's hanging out at home playing Xbox."
-                src={JoeImage}/>
-            <Member 
-                name="Jordan" 
-                role="Cross-Referencer" 
-                bio="Jordan is a Senior Computer Science major at ODU. He has background in Mathematics, Biology, and Organic Chemistry. He is pursuing a Master's Degree in Computer Science and hopes to join a top tech company."
-                src={JordanImage}/>
+            <div className={styles.bottomRow}>
+                <Member 
+                    name="Aaron Breslin" 
+                    role="Backend Developer" 
+                    bio="Aaron is a Senior Computer Science major at ODU. He is also studying for a minor in Computer Engineering. In his free time he enjoys reading books and playing both board games and video games with friends."
+                    src={AaronImage}/>
+                <Member 
+                    name="Jordan" 
+                    role="Documentation Specialist" 
+                    bio="Jordan is a Senior Computer Science major at ODU. He has background in Mathematics, Biology, and Organic Chemistry. He is pursuing a Master's Degree in Computer Science and hopes to join a top tech company."
+                    src={JordanImage}/>
+            </div>
         </div>
     </>
     )
